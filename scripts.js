1f6a78f7ab65f6755cf62647599e858f60eaa9e2
Andrecruzac85 <bomber.andre@gmail.com>
2025-05-21 23:44:01 -0300

    acerto

--- scripts.js
+++ scripts.js
@@ -2,28 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const convertButton = document.querySelector(".convert-button");
 
     function convertValues() {
-        const inputValue = parseFloat(document.querySelector(".value-to-convert").value);
-        console.log(inputValue);
-
-        const dolarToday = 5.65;
-        const converteddolar = inputValue / dolarToday;
-
+     
+        const inputValue = document.querySelector(".value-to-convert").value
+        console.log(inputValue)
+        const dolarToday = 5.65
+        const converteddolar = inputValue / dolarToday 
         const valoraserconvertido = document.querySelector(".real-value");
         const valorconvertido = document.querySelector(".dolar-value");
-
         valoraserconvertido.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
-            currency: "BRL"
-        }).format(inputValue);
-
+            currency: "BRL"}).format(inputValue);
         valorconvertido.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"        
-        }).format(converteddolar);
+        }).format(converteddolar)
+
     }
 
     convertButton.addEventListener("click", convertValues);
 });
-
 
 
